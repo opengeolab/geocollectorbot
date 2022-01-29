389d5e35a576769b7309994c816ad0f631f8e1b8
epessina <edoardopessina.priv@gmail.com>
2022-01-29 18:55:58 +0100

    Coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
+  reporters: ['default', 'jest-junit'],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/utils/testUtils.ts',
